88b241e06c1e4fe844784a21d16a453f62f9c6cb
Thomas-WebDev <k.thomas.k@live.com>
2021-06-07 15:10:13 -0700

    Create MaxAreaOfIslandSolution.java

---
+++ MaxAreaOfIslandSolution.java
@@ -0,0 +1,32 @@
+class MaxAreaOfIslandSolution {
+    // Solution idea: modified Dijkstra's Pathfinder Algorithm -- REVISED found better alternaive algorithm
+    // Find island
+    // Find area of island
+    // Find next island
+    // Find and compare area of next island
+    // Repeat to end of grid
+    
+    int[][] grid;
+    boolean[][] seen;
+
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                  + area(r, c-1) + area(r, c+1));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+}
